21973e3c270c9a5b9f415e9a1f2599ee7ec28690
Emanuele Pigani <pgm4040a@ens.fsi>
2023-04-11 19:54:52 +0200

    TP5_debut

--- src/personnages/Humain.java
+++ src/personnages/Humain.java
@@ -4,11 +4,14 @@ public class Humain {
     private String nom;
     private String boissonFavorite;
     private int argent;
-
+    private int nbConnaissance;
+    private Humain[] conaissances; 
     public Humain(String nom, String boissonFavorite, int argent) {
         this.nom = nom;
         this.boissonFavorite = boissonFavorite;
         this.argent = argent;
+        this.nbConnaissance = 0;
+        this.conaissances = new Humain[30];
     }
 
     public String getNom() {
@@ -42,7 +45,22 @@ public class Humain {
             parler("Je n'ai pas assez d'argent pour acheter " + achat + ".");
         }
     }
-
+    public void faireConnaissanceAvec(Humain autreHumain) {
+    	this.direBonjour();
+    	autreHumain.direBonjour();
+    	this.memoriser(autreHumain);
+    	autreHumain.memoriser(this);
+    }
+    public void memoriser(Humain autreHumain) {
+    	this.nbConnaissance++;
+    	if (this.nbConnaissance < this.conaissances.length) {
+    		this.conaissances[this.nbConnaissance] = autreHumain;
+    	}
+    	else {
+    		
+    	}
+    	autreHumain.memoriser(this);
+    }
     public void parler(String texte) {
         System.out.println("("+nom+")" + " - " + texte);
     }
